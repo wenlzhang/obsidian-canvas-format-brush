4b17b7f1208daf5fb5166c35b3d328bb305c64bb
wz <44930227+wenlzhang@users.noreply.github.com>
2025-03-09 10:33:54 +0100

    Fine tune settings tab

--- src/settingsTab.ts
+++ src/settingsTab.ts
@@ -19,36 +19,7 @@ export class SettingsTab extends PluginSettingTab {
         const { containerEl } = this;
         containerEl.empty();
 
-        // Format Attributes
-        new Setting(this.containerEl).setName("Format attribute").setHeading();
-
-        new Setting(containerEl)
-            .setName("Copy color")
-            .setDesc("Copy the color of the canvas element")
-            .addToggle((toggle: ToggleComponent) =>
-                toggle
-                    .setValue(this.plugin.settings.copyColor)
-                    .onChange(async (value: boolean) => {
-                        this.plugin.settings.copyColor = value;
-                        await this.plugin.saveSettings();
-                    }),
-            );
-
-        new Setting(containerEl)
-            .setName("Copy size")
-            .setDesc("Copy the size of the canvas element")
-            .addToggle((toggle: ToggleComponent) =>
-                toggle
-                    .setValue(this.plugin.settings.copySize)
-                    .onChange(async (value: boolean) => {
-                        this.plugin.settings.copySize = value;
-                        await this.plugin.saveSettings();
-                    }),
-            );
-
-        // UI Settings
-        new Setting(this.containerEl).setName("UI").setHeading();
-
+        // General Settings
         new Setting(containerEl)
             .setName("Show status bar item")
             .setDesc("Show format brush status in the status bar")
@@ -60,6 +31,38 @@ export class SettingsTab extends PluginSettingTab {
                         await this.plugin.saveSettings();
                         // Update status bar visibility
                         this.plugin.updateStatusBar();
+                    }),
+            );
+
+        // Format Attributes
+        new Setting(containerEl)
+            .setName("Format attribute")
+            .setDesc(
+                "These attributes are applied when using the 'Copy format' command, whether through the command palette or the canvas context menu.",
+            )
+            .setHeading();
+
+        new Setting(containerEl)
+            .setName("Copy size")
+            .setDesc("Copy the size from canvas element")
+            .addToggle((toggle: ToggleComponent) =>
+                toggle
+                    .setValue(this.plugin.settings.copySize)
+                    .onChange(async (value: boolean) => {
+                        this.plugin.settings.copySize = value;
+                        await this.plugin.saveSettings();
+                    }),
+            );
+
+        new Setting(containerEl)
+            .setName("Copy color")
+            .setDesc("Copy the color from canvas element")
+            .addToggle((toggle: ToggleComponent) =>
+                toggle
+                    .setValue(this.plugin.settings.copyColor)
+                    .onChange(async (value: boolean) => {
+                        this.plugin.settings.copyColor = value;
+                        await this.plugin.saveSettings();
                     }),
             );
     }
