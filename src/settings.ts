585c79a6eb01cf29fcdca2826bb65aa697eb3782
wz <44930227+wenlzhang@users.noreply.github.com>
2025-03-09 08:39:38 +0100

    Remove hotkeys settings

--- src/settings.ts
+++ src/settings.ts
@@ -3,17 +3,11 @@ import { Plugin } from "obsidian";
 export interface CanvasFormatBrushSettings {
     copyColor: boolean;
     copySize: boolean;
-    enableHotkeys: boolean;
-    copyFormatHotkey: string;
-    pasteFormatHotkey: string;
     showStatusBarItem: boolean;
 }
 
 export const DEFAULT_SETTINGS: CanvasFormatBrushSettings = {
     copyColor: true,
     copySize: true,
-    enableHotkeys: true,
-    copyFormatHotkey: "Ctrl+Shift+C",
-    pasteFormatHotkey: "Ctrl+Shift+V",
     showStatusBarItem: true,
 };
--- src/settingsTab.ts
+++ src/settingsTab.ts
@@ -48,45 +48,6 @@ export class SettingsTab extends PluginSettingTab {
                     }),
             );
 
-        // Hotkeys Section
-        containerEl.createEl("h3", { text: "Hotkeys" });
-
-        new Setting(containerEl)
-            .setName("Enable hotkeys")
-            .setDesc("Enable hotkeys for copy and paste format")
-            .addToggle((toggle: ToggleComponent) =>
-                toggle
-                    .setValue(this.plugin.settings.enableHotkeys)
-                    .onChange(async (value: boolean) => {
-                        this.plugin.settings.enableHotkeys = value;
-                        await this.plugin.saveSettings();
-                    }),
-            );
-
-        new Setting(containerEl)
-            .setName("Copy format hotkey")
-            .setDesc("Hotkey to copy format (set in Obsidian hotkeys settings)")
-            .setDisabled(!this.plugin.settings.enableHotkeys)
-            .addText((text: TextComponent) =>
-                text
-                    .setPlaceholder("Ctrl+Shift+C")
-                    .setValue(this.plugin.settings.copyFormatHotkey)
-                    .setDisabled(true),
-            );
-
-        new Setting(containerEl)
-            .setName("Paste format hotkey")
-            .setDesc(
-                "Hotkey to paste format (set in Obsidian hotkeys settings)",
-            )
-            .setDisabled(!this.plugin.settings.enableHotkeys)
-            .addText((text: TextComponent) =>
-                text
-                    .setPlaceholder("Ctrl+Shift+V")
-                    .setValue(this.plugin.settings.pasteFormatHotkey)
-                    .setDisabled(true),
-            );
-
         // UI Settings Section
         containerEl.createEl("h3", { text: "UI settings" });
 
